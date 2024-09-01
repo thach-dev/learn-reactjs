c6ee17411324299c505eaa0cf9b6ddad9515f821
Le-hoang-thach <Hoangthachnao@gmail.com>
2024-09-01 19:56:11 +0700

    Mouted  Unmouted

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import ToDoList from "./components/ToDoList";
+import Content from "./components/Content";
 function App() {
+  const [show, setShow] = useState(false)
   return (
     <>
+      
+     
+      
       {/* 
-      ToDoList 
-      <ToDoList/>
+        LifeCycle Mounted & Unmounted 
+        Mounted là thời điểm bạn đưa component vào thời điểm sử dụng
+        Unmounted là thời điểm gỡ nó ra
       */}
+      {
+        show && <ToDoList/>
+      }
+      <button onClick={() => setShow(!show)}>Bài  tập To Do List</button>
     </>
   )
 
