387edb590f7f8f13d524622c71581f4bc683112c
Le-hoang-thach <Hoangthachnao@gmail.com>
2024-09-02 20:21:20 +0700

    useEffect Call API

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,22 +1,43 @@
 import { useState } from "react";
-import ToDoList from "./components/ToDoList";
 import Content from "./components/Content";
+// import ToDoList from "./components/ToDoList";
+// khi nào dùng useEffect khi các muốn thực hiện Side Effect thì dùng useEffect
+/*
+  1.Update Dom
+  2.Call API
+  3.Listen Dom Event
+  4.Cleanup
+*/
 function App() {
-  const [show, setShow] = useState(false)
+  
+  //1 useEffect(callback)
+  //- Gọi callback mỗi khi component rerender
+  //- Gọi callback sau khi component thêm element vào DOM
+  //2 useEffect(callback, [])
+  //3 useEffect(callback, [deps])
+  // - Callback sẽ được gọi lại mỗi khi khi deps thay đổi
+  // _____________________
+  //1. Callback luôn được gọi sau khi component mounted
+  const [show, setShow] = useState(false);
   return (
+    
     <>
-      
-     
-      
       {/* 
         LifeCycle Mounted & Unmounted 
         Mounted là thời điểm bạn đưa component vào thời điểm sử dụng
         Unmounted là thời điểm gỡ nó ra
       */}
-      {
+      {/* {
         show && <ToDoList/>
       }
       <button onClick={() => setShow(!show)}>Bài  tập To Do List</button>
+       */}
+      
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content/>}
+
+     
+
     </>
   )
 
--- tiktok/src/components/Content.jsx
+++ tiktok/src/components/Content.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function Content() {
+const tabs = ['posts', 'comments', 'albums'];
+
+function Content() {
+  const [title, setTitle] = useState('');
+  const [post, setPost] = useState([]);
+  const [type, setType] = useState('posts');
+  
+  useEffect(() => {
+    console.log('type changed');
+    
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((res) => res.json())
+      .then((posts) => {
+        console.log('Posts fetched:', posts);
+        setPost(posts);
+      })
+  }, [type]);
+  
   return (
     <div>
-      <h1>Content</h1>
+      {tabs.map(tab => (
+        <button 
+          key={tab}
+          style={type === tab ? 
+            {color: 'red', fontWeight: 'bold', backgroundColor: 'black'} :
+            {}
+          }
+          onClick={() => setType(tab)}
+        >
+          {tab}
+        </button>
+      ))}
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <ul>
+        {post.map(post => (
+          <li key={post.id}>
+            {post.title || post.name}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
+export default Content;
